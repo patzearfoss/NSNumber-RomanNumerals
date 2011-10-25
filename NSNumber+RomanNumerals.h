2867496ad2e8a24e1f154d151d42449fd2caff67
Pat Zearfoss <pzearfoss@gmail.com>
2011-10-25 19:00:17 -0400

    Initial Import

---
+++ NSNumber+RomanNumerals.h
@@ -0,0 +1,14 @@
+//
+//  NSNumber+RomanNumerals.h
+//  SalesBagStandalone
+//
+//  Created by Patrick Zearfoss on 10/19/11.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (RomanNumerals)
+
+- (NSString *)romanNumeral;
+
+@end
